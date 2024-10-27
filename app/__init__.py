633a00f0b2d46bb8d64a4e4351244e17a7582cf8
Porquez <porquez.p@gmx.fr>
2024-10-27 16:11:17 +0100

    Add .keep file to ensure instance directory is tracked

--- app/__init__.py
+++ app/__init__.py
@@ -4,17 +4,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
-from .seeds import seed_natures,seed_beneficiaires,seed_modereglement
+from .seeds import seed_natures, seed_beneficiaires, seed_modereglement
 
 app = Flask(__name__, static_folder='static')
 
-UPLOAD_FOLDER = os.path.join(app.root_path, 'static/uploads')  # Créer un dossier 'uploads' dans le répertoire de votre application
+# Utiliser des variables d'environnement pour la configuration
+UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', os.path.join(app.root_path, 'static/uploads'))
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Définir le chemin de la base de données
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(BASE_DIR, "../instance/protefinance.db")}'
-app.config['SECRET_KEY'] = 'secret_key_here'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', f'sqlite:///{os.path.join(app.root_path, "../instance/protefinance.db")}')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_secret_key_here')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
